9ef8d8a37e2cf34f0069141a43d39ea91969ad20
ReginaMay3 <reginaalcmay@gmail.com>
2025-11-12 14:48:36 -0500

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Balerina from './Balerina.jsx'
+
+function App() {
+
+  return (
+    <>
+      <div>
+       <Balerina />
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/Balerina.jsx
@@ -0,0 +1,27 @@
+
+function Balerina() {
+  return (
+    <>
+        <h1 className="text-xl text-center text-white">Balerina Team</h1>
+        <details class="text-center">
+            <summary> Team Members </summary>
+            <div class="text-lg text-[#a6e7d0] text-center mt-2"> Our talented Balerina development team </div>
+            <details class="text-center">
+                <summary> Bar </summary>
+                <div class="text-lg text-[#a6e7d0] text-center mt-2"> Lead Balerina Developer </div>
+            </details>
+            <details class="text-center">
+                <summary> Leo </summary>
+                <div class="text-lg text-[#a6e7d0] text-center mt-2"> Balerina Developer </div>
+            </details>
+            <details class="text-center">
+                <summary> Gina </summary>
+                <div class="text-lg text-[#a6e7d0] text-center mt-2"> Junior Balerina Developer </div>
+            </details>
+        </details>
+        
+    
+    </>
+)
+}
+export default Balerina
